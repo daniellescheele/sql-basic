466b8f47243c46a38d16421bce3b6d949b148f10
Danielle Scheele <strangeyellowbird@gmail.com>
2016-08-15 11:29:59 -0500

    working query 3

--- non_usa_customers.sql
+++ non_usa_customers.sql
@@ -6,3 +6,13 @@ select c.*
 from Customer c
 where c.Country != 'USA'
 order by c.LastName desc;
+
+
+select
+    i.InvoiceId,
+    i.InvoiceDate,
+    c.FirstName || ' ' || c.LastName as FullName,
+    i.BillingCountry
+from Invoice i, Customer c
+where i.CustomerId = c.CustomerId
+and c.Country = 'Brazil';
